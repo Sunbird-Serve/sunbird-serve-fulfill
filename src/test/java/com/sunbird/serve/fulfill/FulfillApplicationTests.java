4d8a808c8430a87c36f13567e2424f11a862abc2
Sowmya-Raghuram <114737212+Sowmya-Raghuram@users.noreply.github.com>
2023-12-01 16:31:51 +0530

    Fulfill Microservice Setup

---
+++ src/test/java/com/sunbird/serve/fulfill/FulfillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunbird.serve.fulfill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulfillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
